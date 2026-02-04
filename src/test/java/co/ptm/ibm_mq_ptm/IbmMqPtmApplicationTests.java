72e79c4bd18f6a868bdf335bc0b717b168b02848
birbirs <birbirsbirbirs@gmail.com>
2026-02-04 15:46:40 -0600

    teest in active mq

--- src/test/java/co/ptm/ibm_mq_ptm/IbmMqPtmApplicationTests.java
+++ src/test/java/co/ptm/ibm_mq_ptm/IbmMqPtmApplicationTests.java
@@ -1,16 +1,25 @@
 package co.ptm.ibm_mq_ptm;
 
+import org.apache.activemq.broker.BrokerService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = { com.ibm.mq.spring.boot.MQAutoConfiguration.class })
+@ActiveProfiles("test")
 class IbmMqPtmApplicationTests {
+    static BrokerService brokerService;
 
     @LocalServerPort
     private int port;
@@ -18,9 +27,28 @@ class IbmMqPtmApplicationTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        brokerService = new BrokerService();
+        brokerService.setBrokerName("ptmActiveBroker");
+        brokerService.setPersistent(false);
+        brokerService.setUseJmx(true);
+        brokerService.addConnector("vm://embedded-broker");
+        brokerService.start();
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        brokerService.stop();
+    }
+
+
     @Test
     void contextLoads() {
-        String url = "http://localhost:" + port + "/hero";
+        String url = "http://localhost:" + port + "/hero/second";
         String body = """
                 				{
                     "name":"Jeanie",
@@ -33,7 +61,9 @@ class IbmMqPtmApplicationTests {
         HttpEntity<String> stringHttpEntity = new HttpEntity<>(body, httpHeaders);
         ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, stringHttpEntity, String.class);
         assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(exchange.getBody()).contains("power");
+        String receivedMessage = jmsTemplate.receiveAndConvert("ptm").toString();
+        System.out.println(receivedMessage);
+
     }
 
 }
