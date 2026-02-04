bf5643e26e91cc4064e210a11c36c8b9863dd245
birbirs <birbirsbirbirs@gmail.com>
2026-02-04 10:39:38 -0600

    integration test set up

--- src/test/java/co/ptm/ibm_mq_ptm/IbmMqPtmApplicationTests.java
+++ src/test/java/co/ptm/ibm_mq_ptm/IbmMqPtmApplicationTests.java
@@ -1,13 +1,39 @@
 package co.ptm.ibm_mq_ptm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IbmMqPtmApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+        String url = "http://localhost:" + port + "/hero";
+        String body = """
+                				{
+                    "name":"Jeanie",
+                    "power":"Investor Implementation Executive"
+                }
+                """;
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> stringHttpEntity = new HttpEntity<>(body, httpHeaders);
+        ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, stringHttpEntity, String.class);
+        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(exchange.getBody()).contains("power");
+    }
 
 }
